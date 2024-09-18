625a3ab77719da0309ca325d9f52153ee0c2494c
Youngha Park <youngha.kkoti@gmail.com>
2024-09-18 23:59:49 +0900

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import Viewer from "./component/Viewer";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Simple Counter</h1>
+      <section>
+        <Viewer />
+      </section>
+    </div>
+  );
 }
 
 export default App;
