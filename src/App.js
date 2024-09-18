6d2cd1192b898b55da09315412472595acfd2333
Youngha Park <youngha.kkoti@gmail.com>
2024-09-19 01:04:51 +0900

    text

--- src/App.js
+++ src/App.js
@@ -5,17 +5,24 @@ import Controller from "./component/Controller";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
   const handleSetCount = (value) => {
     setCount(count + value);
   };
+  const handleChangeText = (e) => {
+    setText(e.target.value);
+  };
 
   useEffect(() => {
-    console.log("count update:", count);
-  }, [count]);
+    console.log("update:", text, count);
+  }, [text, count]);
 
   return (
     <div className="App">
       <h1>Simple Counter</h1>
+      <section>
+        <input value={text} onChange={handleChangeText} />
+      </section>
       <section>
         <Viewer count={count} />
       </section>
