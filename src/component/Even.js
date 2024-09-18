eda25046bd3ba2c95edc620323bbf0b64cccd9d3
Youngha Park <youngha.kkoti@gmail.com>
2024-09-19 01:33:59 +0900

    unmount control

--- src/component/Even.js
+++ src/component/Even.js
@@ -1,4 +1,12 @@
+import { useEffect } from "react";
+
 function Even() {
+  useEffect(() => {
+    return () => {
+      console.log("Even component unmount");
+    };
+  }, []);
+
   return <div>Current count is even</div>;
 }
 
