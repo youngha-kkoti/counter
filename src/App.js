351b31b8f9e0e46fd60e2eb682f838d76eac797c
Youngha Park <youngha.kkoti@gmail.com>
2024-09-19 11:06:53 +0900

    final commit

--- src/App.js
+++ src/App.js
@@ -2,54 +2,19 @@ import "./App.css";
 import { useState } from "react";
 import Viewer from "./component/Viewer";
 import Controller from "./component/Controller";
-import Even from "./component/Even";
 
 function App() {
   const [count, setCount] = useState(0);
-  const [text, setText] = useState("");
 
   const handleSetCount = (value) => {
     setCount(count + value);
   };
-  const handleChangeText = (e) => {
-    setText(e.target.value);
-  };
-
-  // const didMountRef = useRef(false);
-
-  // useEffect(() => {
-  //   if (!didMountRef.current) {
-  //     didMountRef.current = true;
-  //     return;
-  //   } else {
-  //     console.log("component update!");
-  //   }
-  // });
-
-  // useEffect(() => {
-  //   console.log("component mount");
-  // }, []); // Passing an empty array to useEffect will execute the callback function only at the component's mount time.
-
-  // useEffect(() => {
-  //   const intervalID = setInterval(() => {
-  //     console.log("깜빡");
-  //   }, 1000);
-
-  //   return () => {
-  //     console.log("cleanup");
-  //     clearInterval(intervalID);
-  //   };
-  // });
 
   return (
     <div className="App">
       <h1>Simple Counter</h1>
       <section>
-        <input value={text} onChange={handleChangeText} />
-      </section>
-      <section>
         <Viewer count={count} />
-        {count % 2 === 0 && <Even />}
       </section>
       <section>
         <Controller handleSetCount={handleSetCount} />
